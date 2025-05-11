171ccc5b2519ed270c88aa75d80bb369061a1832
ksharathkrishna <35190023+ksharathkrishna@users.noreply.github.com>
2025-05-11 21:26:35 +0530

    Time: 3 ms (88.2%), Space: 19.1 MB (72.1%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Create adjacency list from prerequisites
         adj = defaultdict(list)
         for u, v in prerequisites:
-            adj[v].append(u)
-        
-        visited = set()
-        cp = [False] * numCourses
+            adj[v].append(u)  # v -> u means u depends on v
 
-        def dfs(n, adj, v, cp):
-            v.add(n)
-            cp[n] = True
-            for a in adj[n]:
-                if a not in v:
-                    if not dfs(a, adj, v, cp): return False
-                elif cp[a]:
+        visited = set()  # Tracks all nodes that have been fully processed
+        currPath = [False] * numCourses  # Tracks nodes in the current DFS path
+
+        # DFS function to detect cycle
+        def dfs(node, adj, visited, currPath):
+            visited.add(node)         # Mark node as visited
+            currPath[node] = True     # Mark node as part of current path
+            
+            for neighbor in adj[node]:
+                if neighbor not in visited:
+                    # Recurse into unvisited neighbor
+                    if not dfs(neighbor, adj, visited, currPath):
+                        return False
+                elif currPath[neighbor]:
+                    # Found a back edge (cycle)
                     return False
-            cp[n]= False
+
+            currPath[node] = False    # Remove node from current path
             return True
 
+        # Check all nodes, in case graph is disconnected
         for i in range(numCourses):
             if i not in visited:
-                if not dfs(i, adj, visited, cp): return False
-        
-        return True
+                if not dfs(i, adj, visited, currPath):
+                    return False  # Cycle detected
+
+        return True  # No cycles found, all courses can be finished
