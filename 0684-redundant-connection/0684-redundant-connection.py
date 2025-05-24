97d681e0fef71f53172a2f4ecc92e3cbc79eeb55
ksharathkrishna <35190023+ksharathkrishna@users.noreply.github.com>
2025-05-24 22:55:51 +0530

    Time: 5 ms (21.48%), Space: 18.3 MB (40.39%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,7 +1,7 @@
 class Solution:
     def find(self, x):
-            while self.parent[x] != 0: x = self.parent[x]
-            return x
+        while self.parent[x] != 0: x = self.parent[x]
+        return x
             
     def union(self, x , y):
         self.parent[self.find(y)] = self.find(x)
@@ -9,9 +9,9 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         sol = []
         self.parent = defaultdict(int)
+
         for u, v in edges:
-            if self.find(u) == self.find(v): 
-                sol = [u,v]
-            else:
-                self.union(u,v)
+            if self.find(u) == self.find(v): sol = [u,v]
+            else: self.union(u,v)
+
         return sol
